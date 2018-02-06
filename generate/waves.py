1874a8cf70278de1336637da5c4d990e9b1f90b4
yonatanbashan <yonatan.bashan@gmail.com>
2018-02-06 20:33:17 +0200

    Restructuring; Adding tri wave

--- generate/waves.py
+++ generate/waves.py
@@ -1,29 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import music
+from scipy import signal
 import generate.sound as sd
 import itertools
 
+
+# Defines a sequence of notes with lengths and amps
+class NoteSheet:
+
+    def __init__(self):
+        self.notes = []
+
+    def add_notes(self, length, notes, amps = []):
+        if notes is list:
+            if not amps:
+                amps = np.ones(len(notes))
+        else:
+            if not amps:
+                amps = [1]
+
+        self.notes.append((length, notes, amps))
+
+    def get_notes(self):
+        return self.notes
+
+
+
 class Modulation:
 
-    def __init__(self, amp, mod_ratio, t = "sin", phase = 0.0, debug = False):
+    def __init__(self, amp, mod_ratio, t = "sin", debug = False, direct_freq = False):
         self.amp = amp
-        self.mod_ratio = mod_ratio
         self.type = t
-        self.phase = phase
         self.debug = debug
+
+        # By default, Modulation frequency will be a ratio of the carrier frequenct
+        self.mod_ratio = mod_ratio
+
+        # If we want the modulation to be constant and not carrier wave frequency dependent. This is good for AM modulation
+        self.direct_freq = direct_freq
 
 class SoundObject:
 
-    def __init__(self, t, phase = 0.0, fm = None, am = None, has_fm = False, has_am = False, has_env = False, env = None):
+    def __init__(self, t, fm = None, am = None, env = None, tri_shape = 0.3):
         self.type = t
-        self.phase = phase
         self.fm = fm
         self.am = am
-        self.has_fm = has_fm
-        self.has_am = has_am
-        self.has_env = has_env
         self.env = env
+        self.tri_shape = tri_shape
+        self.has_fm = False
+        self.has_am = False
+        self.has_env = False
+
+        if fm:
+            self.has_fm = True
+
+        if am:
+            self.has_am = True
+
+        if env:
+            self.has_env = True
+
+        if self.tri_shape > 0.5 or tri_shape < 0.0:
+            raise Exception('tri_shape should be between 0.0 and 0.5')
+
+        if self.has_fm and self.type != 'sin':
+            raise Exception('FM modulation is only available for sin waves')
+
+
+
 
     def set_fm(self, fm):
         self.fm = fm
@@ -39,13 +84,46 @@ class Envelope:
         self.debug = debug
 
 
+# Holds a SoundObject, a NoteSheet, and generates audio from them
 class Channel:
-    def __init__(self, sobj : SoundObject, bit_rate):
+    def __init__(self, sobj : SoundObject, bit_rate, noise = False, noise_amp = 0.0):
         self.sobj = sobj
         self.bit_rate = bit_rate
         self.data = np.zeros(1)
+        self.noise = noise
+        self.noise_amp = noise_amp
+        self.sheet = None
 
-    # Add a note or notes to the channel
+    # Sets the sheet and initializes the data. Whenever this is used, all the previous data is deleted
+    def set_sheet(self, sheet : NoteSheet):
+        self.sheet = sheet
+        self.data = np.zeros(1)
+
+    # Generates the audio from the sheet
+    def generate_sound(self):
+        self.data = np.zeros(1)
+        note_list = self.sheet.get_notes()
+        for note_tuple in note_list:
+            length = note_tuple[0]
+            notes = note_tuple[1]
+            if notes is list:
+                notes = notes[0]
+            amps = note_tuple[2]
+            self.add_notes(length, notes, amps)
+
+
+    # Generates the audio for a note
+    def generate_note(self, note, frames):
+        new_sound = create_sound(self.sobj, music.notes.get_note(note), frames, self.bit_rate)
+        if self.noise:
+            noise = create_noise(frames, self.noise_amp)
+            new_sound = new_sound + noise
+        new_sound = sd.smooth_audio(new_sound)
+
+        return new_sound;
+
+
+    # Adds the audio data of a note(s) to the channel's data
     def add_notes(self, length, notes, amps = []):
         frames = int(self.bit_rate * length)
         if type(notes) == list:
@@ -57,28 +135,25 @@ class Channel:
 
             oscs = []
             for note in notes:
-                new_sound = create_sound(self.sobj, music.notes.get_note(note), frames, self.bit_rate)
-                new_sound = sd.smooth_audio(new_sound)
+                new_sound = self.generate_note(note, frames)
                 oscs.append(new_sound)
                 if not amps:
                     amps.append(1)
 
             new_data = sd.mix_waveforms(osc_list= oscs, amp_list = amps)
         else:
-            new_sound = create_sound(self.sobj, music.notes.get_note(notes), frames, self.bit_rate)
-            new_sound = sd.smooth_audio(new_sound)
+            new_sound = self.generate_note(notes, frames)
             new_data = sd.mix_waveforms(osc_list = [new_sound], amp_list = [1])
 
         self.data = np.append(self.data, new_data)
 
+    # Multiplies the audio data by a given number
     def multiply(self, num = 2):
         new_data = self.data
         for i in range(num - 1):
             self.data = np.append(self.data, new_data)
 
-
-
-
+    # Returns the audio data
     def get_data(self):
         return self.data
 
@@ -93,12 +168,16 @@ class Channel:
 # Creates a wave for modulation (AM or FM)
 def create_mod_wave(dc, base_freq, mod : Modulation, length, bit_rate):
 
-    freq = base_freq * mod.mod_ratio
+    if mod.direct_freq:
+        freq = mod.direct_freq
+    else:
+        freq = base_freq * mod.mod_ratio
+
 
     if mod.type == "sin":
-        mod_wave = create_sin_wave(freq, length, bit_rate, mod.phase)
+        mod_wave = create_sin_wave(freq, length, bit_rate)
     elif mod.type == "square":
-        mod_wave = create_square_wave(freq, length, bit_rate, mod.phase)
+        mod_wave = create_square_wave(freq, length, bit_rate)
 
     data = mod_wave * dc * mod.amp + dc
 
@@ -136,14 +215,38 @@ def create_env_wave(env : Envelope, length, bit_rate):
 
 
 # Creates a square base wave
-def create_square_wave(f, length, bit_rate, phase = 0.0, freq = 0.0):
-    data = create_sin_wave(f, length, bit_rate, phase, freq)
+def create_square_wave(f, length, bit_rate, freq = 0.0):
+    data = create_sin_wave(f, length, bit_rate, freq)
     data = np.sign(data)
+    return data
+
+# Creates a triangle based wave
+def create_tri_wave(length, bit_rate, shape, freq = 0.0):
+
+
+    t = np.linspace(shape, 1-shape, bit_rate/freq) # TODO: Change the 0.4 to 0.6 to parameters of the wave shape
+    t_total = t
+    while len(t_total) < length:
+        t_total = np.append(t_total,t)
+    t_total = t_total[0:length]
+    triangle = signal.sawtooth(2 * np.pi * 5 * t_total, 0.5)
+
+    return triangle
+
+def create_noise(length, amplitude):
+
+    mu = 0.0 #TODO: Maybe change mu to be a parameter?
+    max_sigma = 0.7
+    sigma = amplitude
+    if sigma > max_sigma:
+        sigma = max_sigma
+    data = np.random.normal(mu, sigma, length)
+
     return data
 
 
 # Creates a sin base wave
-def create_sin_wave(f, length, bit_rate, phase = 0.0, f_carrier = 0.0):
+def create_sin_wave(f, length, bit_rate, f_carrier = 0.0):
 
     # Create wave with or without FM
     if type(f) is Modulation:
@@ -151,11 +254,11 @@ def create_sin_wave(f, length, bit_rate, phase = 0.0, f_carrier = 0.0):
         if f.type == "sin":
             mod_wave = np.sin( 2.0 * np.pi * freq * np.arange(length) / bit_rate)
         if f.type == "square":
-            mod_wave = create_square_wave(freq, length, bit_rate, f.phase)
+            mod_wave = create_square_wave(freq, length, bit_rate)
         freq_wave = 2.0 * np.pi * f_carrier + f_carrier * f.amp * mod_wave
-        data = np.sin( np.multiply(freq_wave, np.arange(length)) / bit_rate + phase   )
+        data = np.sin( np.multiply(freq_wave, np.arange(length)) / bit_rate   )
     else:
-        data = np.sin(2.0 * np.pi * np.arange(length) * f / bit_rate + phase)
+        data = np.sin(2.0 * np.pi * np.arange(length) * f / bit_rate)
 
     return data
 
@@ -163,7 +266,6 @@ def create_sin_wave(f, length, bit_rate, phase = 0.0, f_carrier = 0.0):
 # Generates a buffer for a sin wave
 def create_sound(sobj: SoundObject, freq, length, bit_rate, debug = False):
 
-    phase = sobj.phase
     stype = sobj.type
 
     # Replace freq with FM if applied
@@ -174,9 +276,12 @@ def create_sound(sobj: SoundObject, freq, length, bit_rate, debug = False):
 
     # Create wave
     if stype == "sin":
-        data = create_sin_wave(fm_freq, length, bit_rate, phase, freq)
+        data = create_sin_wave(fm_freq, length, bit_rate, freq)
     elif stype == "square":
-        data = create_square_wave(fm_freq, length, bit_rate, phase, freq)
+        data = create_square_wave(fm_freq, length, bit_rate, freq)
+    elif stype == "tri":
+        shape = sobj.tri_shape
+        data = create_tri_wave(length, bit_rate, shape, freq)
 
     # Create envelope, if exists
     if sobj.has_env:
--- synth.py
+++ synth.py
@@ -11,37 +11,34 @@ import time
 
 print ("Mysynth!")
 
-bitRate = 48000.0     #number of frames per second/frameset.
-freq = 530     #Hz, waves per second, 261.63=C4-note.
+bitRate = 48000     #number of frames per second/frameset.
 length = 0.35     #seconds to play sound
 
-if freq > bitRate:
-    bitRate = freq+100
-
-numFrames = int(bitRate * length)
 rate = int(bitRate)
 
-
-fm1_amp = 0.1
+# Define sounds
+fm1_amp = 0.03
 fm1_ratio = 0.5
 fm1_type = "sin"
 
-fm1 = wv.Modulation(amp=fm1_amp, mod_ratio = 1.0)
-am1 = wv.Modulation(0.2, 8.0)
+fm1 = wv.Modulation(amp=fm1_amp, mod_ratio = 0.5)
+am1 = wv.Modulation(amp=0.2, mod_ratio=8.0)
+am2 = wv.Modulation(amp=0.4, mod_ratio=0, direct_freq = 8)
 
 env1 = wv.Envelope(attack=0.07, decay = 0.7, level1 = 1.0, level2 = 0.7, sustain = 0.0)
 
-sound1 = wv.SoundObject("sin", fm = fm1, has_fm = True, am = am1, has_am = True, env = env1, has_env = True)
+sound1 = wv.SoundObject("sin", fm = fm1, am = am1, env = env1)
 sound1.set_fm(wv.Modulation(fm1_amp, fm1_ratio, t=fm1_type))
 
-env2 = wv.Envelope(attack=0.01, decay = 0.03, level1 = 1.0, level2 = 0.15, sustain = 0.0)
-sound2 = wv.SoundObject("sin", has_fm = False, am = am1, has_am = True, env = env2, has_env = True)
+env2 = wv.Envelope(attack=0.01, decay = 0.14, level1 = 1.0, level2 = 0.07, sustain = 0.0)
+sound2 = wv.SoundObject("tri", am = am1, env = env2, tri_shape = 0.01)
 
-sound3 = wv.SoundObject("sin", has_fm = False, am = am1, has_am = True, env = env1, has_env = True)
+sound3 = wv.SoundObject("sin", am = am1, env = env1)
+
+sound4 = wv.SoundObject("sin", fm = fm1, am = am2, env = env1)
 
 
-
-
+# Usage for example
 def play_sound():
 
     # Create audio interface
@@ -50,46 +47,95 @@ def play_sound():
     bar = length
 
     # Create channel for each sound
-    ch1 = wv.Channel(sobj= sound1, bit_rate=bitRate)
+    ch1 = wv.Channel(sobj=sound4, bit_rate=bitRate)
     ch2 = wv.Channel(sobj=sound2, bit_rate=bitRate)
     ch3 = wv.Channel(sobj=sound3, bit_rate=bitRate)
+    
+    sheet1 = wv.NoteSheet()
+    sheet2 = wv.NoteSheet()
+    sheet3 = wv.NoteSheet()
 
-    ch1.add_notes(length=bar, notes="G3")
-    ch1.add_notes(length=bar, notes="B3")
-    ch1.add_notes(length=bar, notes=["D4", "F4"], amps = [1, 1])
+    for i in range(4):
+        sheet1.add_notes(length=bar, notes="G4")
+        sheet1.add_notes(length=bar, notes="B4")
+        sheet1.add_notes(length=bar, notes=["D5", "F5"], amps = [1, 1])
 
-    ch1.multiply(4)
+    for i in range(2):
+        sheet1.add_notes(length=bar, notes="C4")
+        sheet1.add_notes(length=bar, notes="E4")
+        sheet1.add_notes(length=bar, notes=["G4", "C5"], amps = [1, 1])
 
-    ch2.add_notes(length=bar, notes="G5")
-    ch2.add_notes(length=bar, notes="F#5")
-    ch2.add_notes(length=bar, notes="F5")
-    ch2.add_notes(length=bar, notes="E5")
-    ch2.add_notes(length=bar/3, notes="D#5")
-    ch2.add_notes(length=bar/3, notes="E5")
-    ch2.add_notes(length=bar/3, notes="D#5")
-    ch2.add_notes(length=bar, notes="D5")
+    for i in range(2):
+        sheet1.add_notes(length=bar, notes="B3")
+        sheet1.add_notes(length=bar, notes="E4")
+        sheet1.add_notes(length=bar, notes=["G4", "C5"], amps = [1, 1])
 
-    ch2.add_notes(length=bar, notes="D5")
-    ch2.add_notes(length=bar, notes="C#5")
-    ch2.add_notes(length=bar, notes="D5")
-    ch2.add_notes(length=bar, notes="D#5")
-    ch2.add_notes(length=bar, notes="D5")
-    ch2.add_notes(length=bar, notes="C#5")
+    for i in range(2):
+        sheet1.add_notes(length=bar, notes="A3")
+        sheet1.add_notes(length=bar, notes="E4")
+        sheet1.add_notes(length=bar, notes=["G4", "C5"], amps = [1, 1])
 
-    ch3.add_notes(length=bar * 3, notes="G2")
-    ch3.add_notes(length=bar * 3, notes="D#2")
-    #ch3.add_notes(length=bar * 3, notes="G2")
-    #ch3.add_notes(length=bar * 3, notes="B2")
+    for i in range(2):
+        sheet1.add_notes(length=bar, notes="B3")
+        sheet1.add_notes(length=bar, notes="D4")
+        sheet1.add_notes(length=bar, notes=["F4", "B4"], amps = [1, 1])
 
+
+    #ch1.multiply(4)
+
+    sheet2.add_notes(length=bar*2, notes="G5")
+    sheet2.add_notes(length=bar, notes="G5")
+    sheet2.add_notes(length=bar*2, notes="F5")
+    sheet2.add_notes(length=bar, notes="F5")
+    sheet2.add_notes(length=bar*2, notes="E5")
+    sheet2.add_notes(length=bar, notes="E5")
+    sheet2.add_notes(length=bar, notes="D5")
+    sheet2.add_notes(length=bar, notes="E5")
+    sheet2.add_notes(length=bar, notes="A5")
+
+    sheet2.add_notes(length=bar*3, notes="C5")
+    sheet2.add_notes(length=bar, notes="C5")
+    sheet2.add_notes(length=bar, notes="B4")
+    sheet2.add_notes(length=bar, notes="C5")
+    sheet2.add_notes(length=bar*3, notes="D5")
+    sheet2.add_notes(length=bar, notes="D5")
+    sheet2.add_notes(length=bar, notes="C5")
+    sheet2.add_notes(length=bar, notes="D5")
+    sheet2.add_notes(length=bar*3, notes="E5")
+    sheet2.add_notes(length=bar, notes="E5")
+    sheet2.add_notes(length=bar, notes="D5")
+    sheet2.add_notes(length=bar, notes="E5")
+    sheet2.add_notes(length=bar*6, notes="F5")
+
+
+    sheet3.add_notes(length=bar * 12, notes="G2")
+    sheet3.add_notes(length=bar * 6, notes="C3")
+    sheet3.add_notes(length=bar * 6, notes="B2")
+    sheet3.add_notes(length=bar * 6, notes="A2")
+    sheet3.add_notes(length=bar * 3, notes="F2")
+    sheet3.add_notes(length=bar * 3, notes="B2")
+
+    ch1.set_sheet(sheet1)
+    ch2.set_sheet(sheet2)
+    ch3.set_sheet(sheet3)
+
+    ch1.generate_sound()
+    ch2.generate_sound()
+    ch3.generate_sound()
 
     oscs = [ch1.get_data(), ch2.get_data(), ch3.get_data()]
-    amps = [0.5, 1, 0.7]
+    amps = [0.5, 0.5, 0.5]
+
 
     master_output = sd.mix_waveforms(oscs, amps)
 
     aw.append_sound(master_output)
 
     aw.flush_audio(rate, file='pyAudioOutput.wav')
+
+    #plt.figure()
+    #plt.plot(master_output[4000:12000])
+    #plt.show()
 
 #g4_data = sd.prepare_audio(g4, debug=False)
 #plt.figure()
